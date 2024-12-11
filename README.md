935536345eb361474ede24bd14aedc55cde4b81d
Roshan Dahal <roshan@window2theworld.org>
2024-12-11 16:46:16 +0000

    EduKUDO Test Task initialised by Tej Dahal

---
+++ README.md
@@ -0,0 +1,118 @@
+# EduKUDO Task
+
+> **Introduction**  
+This task, created by **Tej Dahal**, is a Laravel-based project utilizing Inertia.js, Vue.js 3, Vite.js, Docker, and Sail. Follow the guide below to set up the application.
+
+> **Note:** Docker and Docker Compose are required to run this application.
+
+---
+
+## Installation Guide
+
+### Prerequisites
+Ensure you have Docker and Docker Compose installed on your system before proceeding.
+
+
+
+### Getting Started
+
+#### Clone the Repository
+
+```bash
+git clone git@github.com:dahaltn/eduKudo.git
+cd ./eduKudo
+
+```
+Next, we can install eduKudo test project with these **4 simple steps**:
+
+### 1. Install php composer dependencies.
+
+Run this command from the project folder after cloning the repo.
+
+```bash
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$(pwd):/var/www/html" \
+    -w /var/www/html \
+    laravelsail/php84-composer:latest \
+    composer install --ignore-platform-reqs
+```
+Alias sail command
+```Bash
+alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
+```
+
+### 2. Duplicate the `.env.example` file and name it to `.env`
+
+```Bash
+cp .env.example .env
+```
+
+Then, open up the `.env` file and update your `DB_DATABASE`, `DB_USERNAME` and `DB_PASSWORD` in the appropriate fields. Also update the `APP_URL` to the URL of your application.
+
+``` Database setup is optional for this test task. ```
+
+```env
+APP_URL=http://localhost
+
+DB_CONNECTION=mysql
+DB_HOST=mysql
+DB_PORT=3306
+DB_DATABASE=laravel
+DB_USERNAME=sail
+DB_PASSWORD=password
+```
+
+Note: If you forget to create the .env file prior to executing `sail up -d`, the database credentials will default to the values specified in the example above
+
+### 3. Build and run containers
+
+Run sail build --no-cache to build fresh containers.
+
+*Note: No need to run this command if you have already build images.*
+
+```bash
+sail build --no-cache
+```
+Run containers
+```bash
+sail up -d
+```
+
+### 4. Install Dependencies
+
+We need to install npm dependencies,  to install all packages, please run commands below.
+```bash
+## Install php dependencies.
+sail composer install
+```
+
+```bash
+## Install js dependencies.
+sail npm install
+```
+
+```bash
+## Compile JS files.
+sail npm run build
+sail npm run dev (To watch live changes)
+```
+### 4. Install Migration (optional)
+
+You must migrate database schema into database, which we can accomplish by running the following command:
+```php
+sail artisan migrate
+```
+<br>
+
+🎉 And that's it! You will now be able to visit your URL and see the application up and running.
+
+
+### Watch, Learn, and Build
+
+Everytime we make changes to JS files it must be compiled to be able to reflect the JS changes, please use below command.
+
+```bash
+sail npm run dev
+```
+**Note:** if any reason ``sail`` command not working, you can use run ``./vendor/bin/sail <command to run>`` from project folder 
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,79 @@
+<script setup>
+import {Head, Link} from '@inertiajs/vue3';
+import ParentComponent from "@/EduKudoComponents/ParentComponent.vue";
+import LessEditorComponent from "@/EduKudoComponents/LessEditorComponent.vue";
+
+</script>
+
+<template>
+    <Head title="Welcome EduKUDO Test"/>
+
+    <div class="flex flex-col items-center min-h-screen bg-gray-100">
+        <!-- Navigation Bar -->
+        <nav v-if="canLogin" class="w-full bg-gray-200 shadow-md py-4 px-6 flex justify-center">
+            <div class="w-4/5 flex justify-end">
+                <Link
+                    v-if="$page.props.auth.user"
+                    :href="route('dashboard')"
+                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70"
+                >
+                    Dashboard
+                </Link>
+
+                <template v-else>
+                    <Link
+                        :href="route('login')"
+                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+                    >
+                        Log in
+                    </Link>
+
+                    <Link
+                        v-if="canRegister"
+                        :href="route('register')"
+                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+                    >
+                        Register
+                    </Link>
+                </template>
+            </div>
+        </nav>
+
+        <!-- Main Content -->
+        <main class="w-4/5 bg-white shadow-lg rounded-lg p-8 mt-6">
+            <h1 class="text-2xl font-bold text-center">Welcome to EduKUDO Test</h1>
+            <p class="mt-4 text-center p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci delectus
+                dolores facere fugiat magni
+                omnis reiciendis veniam voluptas. Ab autem delectus expedita fuga id laborum minima mollitia odio
+                officia repellendus.</p>
+
+
+            <!-- Dev Tech Challenge Section -->
+            <div class="w-full bg-gray-50 shadow-inner rounded-md p-6 mt-8">
+                <h2 class="text-lg font-semibold mb-4">Task 1: Parent & child components communication:</h2>
+                <ParentComponent />
+            </div>
+
+            <!-- Task 2 Section -->
+            <div class="w-full bg-gray-50 shadow-inner rounded-md p-6 mt-8">
+                <h2 class="text-lg font-semibold mb-4">Task 2: LESS and HTML Preview ( path: resources/js/EduKudoComponents/LessEditorComponent.vue)</h2>
+                <LessEditorComponent />
+            </div>
+
+
+            <div class="flex justify-center mt-6">
+                <p class="text-center font-bold">Task completed by: <span class="italic">
+        Tej Dahal </span></p>
+            </div>
+
+        </main>
+
+
+
+        <!-- Footer -->
+        <footer
+            class="py-8 text-center text-sm text-black">
+            EduKUDO Dev Challenge {{ new Date().getFullYear() }}, all rights reserved.
+        </footer>
+    </div>
+</template>
